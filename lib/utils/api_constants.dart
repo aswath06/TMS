1515eef3f495a8be7278f34b5eaad8818cdcd9ce
aswath06 <maswath55@gmail.com>
2026-04-15 13:39:12 +0530

    driver

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "https://tripzo.bitsathy.ac.in";
+  static const String baseUrl = "https://18x50gz9-8055.inc1.devtunnels.ms";
 
   // DevTunnels bypass header
   static const String bypassHeaderKey = "X-Tunnel-Skip-Anti-Phishing-Page";
