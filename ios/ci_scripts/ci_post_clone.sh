965eed6c36c21fc38b3370f6125f3dd35e2e6a27
aswath06 <maswath55@gmail.com>
2026-06-24 12:16:50 +0530

    Add Xcode Cloud post clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
-
+# Fail this script if any command fails.
+set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
-
-export FLUTTER_ROOT="$HOME/flutter"
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $FLUTTER_ROOT
-export PATH="$FLUTTER_ROOT/bin:$PATH"
-
-# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+# Install Flutter using git
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+# Install iOS dependencies
 flutter precache --ios
-
 # Install Flutter dependencies.
 flutter pub get
-
 # Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
-brew install cocoapods
-
+HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
-
+cd ios && pod install
 exit 0
